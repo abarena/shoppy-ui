24e4e026b63ea0f0cf2780e1dd45b74f6537187e
abarena <angelbarena89@gmail.com>
2024-09-11 22:22:54 -0600

    first commit

---
+++ app/auth/signup/create-user.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { post } from "@/app/common/util/fetch";
+import { redirect } from "next/navigation";
+import { FormResponse } from "@/app/common/interfaces/form-response.interface";
+
+export default async function createUser(_prevState: FormResponse, formData: FormData) {
+  const { error } = await post("users", formData);
+  if (error) {
+    return { error }
+  }
+  redirect("/");
+}
---
+++ app/common/util/fetch.ts
@@ -0,0 +1,35 @@
+import { API_URL } from "@/app/common/constants/api";
+import { getErrorMessage } from "./errors";
+import { cookies } from "next/headers";
+
+export const getHeaders = () => ({
+  Cookie: cookies().toString()
+})
+
+export const post =  async (path: string, data: FormData | object) => {
+  const body = data instanceof FormData ? Object.fromEntries(data): data;
+  const res = await fetch(`${API_URL}/${path}`, {
+    method: "POST",
+    headers: {"Content-Type": "application/json", ...getHeaders()},
+    body:  JSON.stringify(body),
+  })
+  const parsedRes = await res.json();
+
+  if (!res.ok) {
+    return { error: getErrorMessage(parsedRes)};
+  }
+  return { error: "", data: parsedRes};
+}
+
+export const get = async <T>(
+    path: string,
+    tags?: string[],
+    params?: URLSearchParams,
+  ) => {
+  const url = params ? `${API_URL}/${path}?` + params : `${API_URL}/${path}`; 
+  const res = await fetch(url, {
+    headers: { ...getHeaders() },
+    next: {tags}
+  });
+  return res.json() as T;
+}
